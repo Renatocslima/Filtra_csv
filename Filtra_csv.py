3c37b633d323274af69a75c917c88d7f6af85956
Renatocslima <133530335+Renatocslima@users.noreply.github.com>
2023-08-21 15:12:35 -0300

    Update Filtra_csv.py
    Atualizando para funcionar fora do colab

--- Filtra_csv.py
+++ Filtra_csv.py
@@ -1,16 +1,16 @@
-#Bibliotecas utilizadas
-import pandas as pd
-import io
+# Bibliotecas utilizadas
 
-#Faça uploud do csv original
-uploaded = files.upload()
-#Colocando os dados em uma variavel
-uplcsv=io.BytesIO(uploaded['arquivo_que_deve_ser_lido.csv'])
-#Transfomando esses dados da variavel em um DataFrame
-df2 = pd.read_csv(uplcsv, sep=';', header=0, encoding = "ISO-8859-1")
-#Fazendo o Filtro e salvando em uma nova variavel
-dfCon=df2.loc[df2["Nome_da_coluna"]=="O_que_irá_filtrar"]
-#Classificando os dados
-dfInd=dfCon.set_index('Nome da coluna para funcionar como indice')
-#Salvando em um novo arquivo
-dfInd.to_csv("Nome_do_novo_arquivo.csv",sep=';',encoding = "ISO-8859-1")
+import pandas as pd
+
+# Colocando o nome do arquivo em uma variável
+nome_arquivo = "endereço_do_arquivo.csv"
+
+# Transformando esses dados da variável em um DataFrame
+df2 = pd.read_csv(nome_arquivo, sep=';', header=0, encoding="ISO-8859-1")
+# Fazendo o Filtro e salvando em uma nova variável
+dfCon = df2.loc[df2["coluna_do_filtro"] == "filtro"]
+# Classificando os dados
+dfInd = dfCon.set_index('seta uma coluna como indice')
+# Salvando em um novo arquivo
+dfInd.to_csv("nome_do_novo_arquivo.csv", sep=';', encoding="ISO-8859-1")
+
